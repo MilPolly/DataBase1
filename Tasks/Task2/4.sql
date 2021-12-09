3045f1f91c0be0263ee169df793322226acafea1
MilPolly <polinamilash@yandex.ru>
2021-12-09 18:49:26 +0300

    Лабораторная работа 2 #5

---
+++ Tasks/Task2/4.sql
@@ -0,0 +1,9 @@
+--Вывести пол, где средний возраст = 42 из таблицы HumanResources.Employee (не учитывать пустые значения).
+SELECT Gender, AVG(DATEDIFF(YEAR, BirthDate, GETDATE())) as Age
+FROM HumanResources.Employee
+
+WHERE Gender IS NOT NULL AND BirthDate IS NOT NULL
+
+GROUP BY Gender
+
+HAVING AVG(DATEDIFF(YEAR, BirthDate, GETDATE())) = 42;
