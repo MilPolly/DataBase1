b82ee2f4d4bba6c831ccdf29a6ebed750e90471c
MilPolly <polinamilash@yandex.ru>
2021-12-14 18:47:12 +0300

    ----

--- Tasks/Task2/4.sql
+++ Tasks/Task2/4.sql
@@ -1,9 +1,6 @@
 --Вывести пол, где средний возраст = 42 из таблицы HumanResources.Employee (не учитывать пустые значения).
 SELECT Gender, AVG(DATEDIFF(YEAR, BirthDate, GETDATE())) as Age
 FROM HumanResources.Employee
-
 WHERE Gender IS NOT NULL AND BirthDate IS NOT NULL
-
 GROUP BY Gender
-
 HAVING AVG(DATEDIFF(YEAR, BirthDate, GETDATE())) = 42;
--- Tasks/Task2/5.sql
+++ Tasks/Task2/5.sql
@@ -1,4 +1,6 @@
---Вывести EmployeeID, ShipDate и среднее по SubTotal для всевозможных EmployeeID с одинаковой ShipDate, предусмотреть вывод общего среднего StandardPrice за ShipDate из таблицы Purchasing.PurchaseOrderHeader. (Использовать CUBE)
+--Вывести EmployeeID, ShipDate и среднее по SubTotal для всевозможных EmployeeID с одинаковой ShipDate, 
+--предусмотреть вывод общего среднего StandardPrice за ShipDate
+--из таблицы Purchasing.PurchaseOrderHeader. (Использовать CUBE)
 SELECT EmployeeID, ShipDate, AVG(SubTotal) as  StandardPrice 
 FROM Purchasing.PurchaseOrderHeader
 GROUP BY CUBE(EmployeeID, ShipDate);
