a20853beb481d688aba9c1032faba1de9cb979ce
MilPolly <polinamilash@yandex.ru>
2021-12-28 15:34:33 +0300

    Курсовая работа #18

---
+++ Tasks/KR/5.sql
@@ -0,0 +1,4 @@
+--5.Выдавать список наиболее угоняемых автомобилей по марке.
+SELECT b.ModelID, COUNT(*) as HijackingCount FROM Hijacking as a
+JOIN Car as b ON a.CarID = b.CarID
+GROUP BY b.ModelID
